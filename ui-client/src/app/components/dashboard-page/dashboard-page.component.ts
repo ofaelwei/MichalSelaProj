1455e230e3b942198bd8bcada48090b0283f821b
ofaelweiss <orit.faelweiss@cyberark.com>
2024-05-23 00:55:59 +0300

    + event dto

--- ui-client/src/app/components/dashboard-page/dashboard-page.component.ts
+++ ui-client/src/app/components/dashboard-page/dashboard-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventDTO } from '../../models/event-dto'; // Import the EventDTO class
+  
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.css'
 })
-export class DashboardPageComponent {
+export class DashboardPageComponent implements OnInit {
 
+  event: EventDTO | undefined; // Declare a variable of type EventDTO
+
+
+  ngOnInit() {
+
+
+          // Initialize the event variable
+    this.event = {
+      id: 1,
+      userId: 'user123',
+      eventName: 'login',
+      eventTime: '2024-05-22T12:00:00.000Z'
+    };
+  }
 }
---
+++ ui-client/src/app/models/event-dto.ts
@@ -0,0 +1,6 @@
+export interface EventDTO {
+  id: number;
+  userId: string;
+  eventName: string;
+  eventTime: string;
+}
